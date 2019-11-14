bff427780f3fcc581a5604029c271a47652cbc8e
geoffreyottoy <geoffrey.ottoy@kuleuven.be>
2019-11-14 13:34:35 +0100

    Update main.cpp
    lora test application (working)

--- src/main.cpp
+++ src/main.cpp
@@ -27,7 +27,7 @@ void setup() {
   //nm.runConfigMode(true); // run config forever
 
   // join lora network
-  //lora.join();
+  lora.join();
 }
 
 void loop() {
@@ -36,8 +36,8 @@ void loop() {
 
   // aggregate by using payload size (return value of NodeManager::payloadAvailable())
   uint8_t pSize = nm.payloadAvailable();
-  if(pSize){
-    DEBUG.println("\nMAIN APP: Data available!");
+  if(pSize > 10){
+    DEBUG.println("\nMAIN APP: Enough data available!");
     if(nm.getLoraPayload(buf, 16)){
       DEBUG.print("Payload: ");
       for(uint8_t i=0; i<pSize; i++){
@@ -51,10 +51,33 @@ void loop() {
     else{
       DEBUG.println("Empty payload.\n");
     }
-  }
 
   // TODO: make RN2483 sleep as well?
+/* From the reference guide
+sys sleep <length><length>: decimal number representing the number of milliseconds the system is put to sleep, 
+from 100 to 4294967296.
+
+Response:
+- ok after the system gets back from Sleep mode
+- invalid_param if the length is not valid
+
+This command puts the system to sleep for the specified number of milliseconds.
+
+The module can be forced to exit from sleep by sending the UART a Break condition followed by a 0x55 character.
+Forcing the module from sleep in the manner also triggers the UART auto baud detection. The module will adjust 
+the UART baud rate to match the baud rate at which the 0x55 character was sent. Refer to the note box in 1.4 
+“UART Interface”.Example: sys sleep 120// Puts the system to sleep for 120 ms.
+
+=== G.O. says: 
+To make this work for us:
+  -> make it go to sleep for a very long time
+  -> provide means to skip response (if not "invalid_param")
+  -> provide means to capture "ok" on wake
+  -> and provide means for sending "break condition" etc. 
+*/
+
   // Handle errors due to channel not free
-  //lora.sendData(testdata, 4);
-  //testdata[3]++;
+    lora.sendData(buf, pSize);
+  }
+
 }

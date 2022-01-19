d2a4c5618d7fe1100d5432a5ead02eb6537005b2
Geoffrey Ottoy <geoffrey.ottoy@kuleuven.be>
2022-01-19 17:08:31 +0100

    Update device_settings.h

--- firmware/src/device_settings.h
+++ firmware/src/device_settings.h
@@ -20,21 +20,20 @@
 #ifndef __DEVICE_SETTINGS_H__
 #define __DEVICE_SETTINGS_H__
 
+
+// TODO: reorganise settings file
+
+#define IWAST_VERSION "IWAST v2022_January_001\0"
+
 /*****************************************************************************************
  * CONFIGURE THIS!
  *****************************************************************************************/
 
-#define MOTHERBOARD_ID				0x474F
+//#define MOTHERBOARD_ID				0x474F
 #define LORA_MAX_PAYLOAD_SIZE		48
 #define LORA_ACCUMULATE_THRESHOLD	30
 
 /* LoRaWAN section ***********************************************************************/
-
-/* OVER_THE_AIR_ACTIVATION:
- * 	- set to 1 to use the Over-the-Air activation procedure
- * 	- set to 0 to use the Personalization activation procedure
- */
-#define OVER_THE_AIR_ACTIVATION		0
 
 /*
  * SET default data rate
@@ -55,30 +54,6 @@
 #define ENABLE_ADR					true
 #define CONFIRMED_MESSAGES_ALLOWED  true
 
-#if OVER_THE_AIR_ACTIVATION == 1
-// Copy your settings here
-/* 64-bit device ID */
-#define LORAWAN_DEVICE_EUI			"YOUR_DEVICE_EUI_"
-/* 64-bit application ID */
-#define LORAWAN_APPLICATION_EUI     "YOUR_APP_EUI____"
-/* 128-bit AES key */
-#define LORAWAN_APPLICATION_KEY     "YOUR_SECRET_AES_APPLICATION_KEY_"
-// Don't change
-#define JOIN_MECHANISM				OTAA
-
-#elif OVER_THE_AIR_ACTIVATION == 0
-// Copy your settings here
-/* 32-bit device address */
-#define LORAWAN_DEVICE_ADDRESS     	"YOUR_DEVICE_ADDRESS_"
-/* 128-bit AES key */
-#define LORAWAN_NWKSKEY             "YOUR_SECRET_AES_NTW_SESSION_KEY_"
-/* 128-bit AES key */
-#define LORAWAN_APPSKEY             "YOUR_SECRET_AES_APP_SESSION_KEY_"
-// Don't change
-#define JOIN_MECHANISM				ABP
-#else
-#error "OVER_THE_AIR_ACTIVATION has the wrong value"
-#endif
 /*****************************************************************************************
  * END OF CONFIGURABLE SECTION
  *****************************************************************************************/
@@ -88,7 +63,20 @@
 /*****************************************************************************************
  * DO NOT CHANGE !
  *****************************************************************************************/
-#if OVER_THE_AIR_ACTIVATION == 1
+
+// Copy your settings here
+/* 64-bit device ID */
+#define LORAWAN_DEVICE_EUI			"4957415354200000" // last two bytes are set by the configurator tool
+/* 64-bit application ID */
+#define LORAWAN_APPLICATION_EUI     "4957415354444556" // fixed
+/* 128-bit AES key */
+#define LORAWAN_APPLICATION_KEY     "00000000000000000000000000000000" // key is set by the configurator tool
+// Don't change
+#define JOIN_MECHANISM				OTAA
+
+#define EUI_LENGTH 16
+#define KEY_LENGTH 32
+
 #define LORA_INIT_MY_DEVICE                                         \
 {	JOIN_MECHANISM,						/* Activation mechanism */	\
 	DEFAULT_DATA_RATE,					/* Data Rate on start-up */ \
@@ -100,18 +88,5 @@
 	"",									/* Network session key*/ 	\
 	"",									/* App session key*/ 		\
 }
-#else
-#define LORA_INIT_MY_DEVICE                                         \
-{	JOIN_MECHANISM,						/* Activation mechanism */	\
-	DEFAULT_DATA_RATE,					/* Data Rate on start-up */ \
-	(ENABLE_ADR && CONFIRMED_MESSAGES_ALLOWED),	/* Use ADR or not (bool) */ \
-	"",                 				/* Device EUI */            \
-	"",                					/* Application EUI */       \
-	"",									/* Application key */       \
-	LORAWAN_DEVICE_ADDRESS,				/* Device address */ 		\
-	LORAWAN_NWKSKEY, 					/* Network session key*/ 	\
-	LORAWAN_APPSKEY, 					/* App session key*/ 		\
-}
-#endif
 
 #endif /* __DEVICE_SETTINGS_H__ */

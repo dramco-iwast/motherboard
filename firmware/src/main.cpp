356495c4479e752394904c901e944bd98d1fdfb6
Geoffrey Ottoy <geoffrey.ottoy@kuleuven.be>
2022-02-25 11:25:44 +0100

    Update main.cpp

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -61,6 +61,9 @@ void setup(){
 
   // get device specific settings
   nm.getLoraSettings(&settings);
+#if (ENABLE_ADR && CONFIRMED_MESSAGES_ALLOWED) == true
+  settings.enableADR = true;
+#endif
 
   // configure lora (no networking yet)
   lora.begin(settings, &led);
